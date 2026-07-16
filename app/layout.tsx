beed33f4d4b407bb51b58b2ac51289deead61b25
mras1211-web <mras1211@gmail.com>
2026-07-16 03:59:08 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Source_Serif_4, IBM_Plex_Sans, IBM_Plex_Mono } from "next/font/google";
+import { Source_Serif_4, IBM_Plex_Sans_Arabic, IBM_Plex_Mono } from "next/font/google";
 import "./globals.css";
 
 const display = Source_Serif_4({ subsets: ["latin"], variable: "--font-display", weight: ["500", "600"] });
-const sans = IBM_Plex_Sans({ subsets: ["latin", "arabic"], variable: "--font-sans", weight: ["400", "500", "600"] });
+const sans = IBM_Plex_Sans_Arabic({ subsets: ["arabic", "latin"], variable: "--font-sans", weight: ["400", "500", "600"] });
 const mono = IBM_Plex_Mono({ subsets: ["latin"], variable: "--font-mono", weight: ["400", "500"] });
 
 export const metadata: Metadata = {
